d8448d5678dfa0e6e3ca8f00d5ce426da7d59690
Renda Gezag Pawestri <renda.pawestrig@student.uns.ac.id>
2024-06-28 09:44:49 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineCustomElements } from "@ionic/pwa-elements/loader"
+defineCustomElements(window)
+</script>
+
+<style>
+@import url("~/assets/css/base.css");
+@import url("primeicons/primeicons.css");
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
